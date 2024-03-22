f00dfb6d27bf4891a298aafd0253453ad10c4194
siby.augusty <sibyaugusty999@gmail.com>
2024-03-22 22:06:25 +0530

    css files name changed, directory changed, slider for logos added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,39 @@
 $(document).ready(function () {
+
+    $(".language-slider").slick({
+        infinite: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 2500,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+
     function applyScrollReveal() {
         if ($(window).width() < 576) {
             $(".project").addClass("scroll-reveal");
@@ -19,6 +54,7 @@ $(document).ready(function () {
     $(".menu-icon").click(function () {
         $("nav ul").toggleClass("active-menu");
     });
+
 
     // scroll slide up animation
     ScrollReveal().reveal(".scroll-reveal", {
