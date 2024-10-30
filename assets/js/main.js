56c8480ab3641d17f0700ed77628857b025615ac
sibyaugusty <sibyaugusty999@gmail.com>
2024-10-30 22:04:15 +0530

    removed about and project sections and created new about section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,109 +1,112 @@
 $(document).ready(function () {
+  setTimeout(function () {
+    $(".carousel-container").addClass("active");
+    $(".social-media").addClass("active");
+  }, 4000);
 
-    $(".language-slider").slick({
-        infinite: true,
-        arrows: false,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2500,
-        speed: 2500,
-        pauseOnHover: false,
-        focusOnSelect: true,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                },
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                },
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    autoplaySpeed: 100,
-                    speed: 2000,
-                },
-            },
-        ],
-    });
+  $(".language-slider").slick({
+    infinite: true,
+    cssEase: "linear",
+    arrows: false,
+    slidesToShow: 5,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 2000,
+    pauseOnHover: false,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  });
 
-    function applyScrollReveal() {
-        if ($(window).width() < 576) {
-            $(".project").addClass("scroll-reveal");
-            $(".projects-grid").removeClass("scroll-reveal");
-            $(".button-container").addClass("scroll-reveal");
-        } else {
-            $(".projects-grid").addClass("scroll-reveal");
-            $(".project").removeClass("scroll-reveal");
-        }
+  function applyScrollReveal() {
+    if ($(window).width() < 576) {
+      $(".project").addClass("scroll-reveal");
+    } else {
+      $(".project").removeClass("scroll-reveal");
     }
+  }
 
-    // Apply scroll reveal on page load
-    applyScrollReveal();
+  // Apply scroll reveal on page load
+  applyScrollReveal();
 
-    // Re-evaluate on window resize
-    $(window).resize(applyScrollReveal);
-    //expanding header for responsive devices
-    $(".menu-icon").click(function () {
-        $("nav ul").toggleClass("active-menu");
-    });
+  // Re-evaluate on window resize
+  $(window).resize(applyScrollReveal);
+  
+  //expanding header for responsive devices
+  $(".menu-icon").click(function () {
+    $("nav ul").toggleClass("active-menu");
+  });
 
+  // scroll slide up animation
+  ScrollReveal().reveal(".scroll-reveal", {
+    delay: 200,
+    distance: "80px",
+    origin: "bottom",
+    duration: 1000,
+    easing: "cubic-bezier(0.5, 0, 0, 1)",
+    reset: true,
+  });
 
-    // scroll slide up animation
-    ScrollReveal().reveal(".scroll-reveal", {
-        delay: 200,
-        distance: "80px",
-        origin: "bottom",
-        duration: 1000,
-        easing: "cubic-bezier(0.5, 0, 0, 1)",
-        reset: true,
-    });
-
-    //scroll header animation
-    $(window).scroll(function () {
-        if ($(window).scrollTop()) {
-            $("header").addClass("shrink");
-            $(".logo").addClass("shrink");
-        } else {
-            $("header").removeClass("shrink");
-            $(".logo").removeClass("shrink");
-        }
-    });
-
-    var professions = ["Full-Stack Developer", "Web Developer", "Software Engineer", "Front-End Developer"];
-    var currentIndex = 0;
-    var currentCharIndex = 0;
-    var element = $("#profession");
-
-    function typeEffect() {
-        if (currentCharIndex < professions[currentIndex].length) {
-            element.text(element.text() + professions[currentIndex][currentCharIndex++]);
-            setTimeout(typeEffect, 100); // Typing speed in milliseconds
-        } else {
-            setTimeout(deleteEffect, 2000); // Wait for 2 seconds before starting to delete
-        }
+  //scroll header animation
+  $(window).scroll(function () {
+    if ($(window).scrollTop()) {
+      $("header").addClass("shrink");
+      $(".logo").addClass("shrink");
+    } else {
+      $("header").removeClass("shrink");
+      $(".logo").removeClass("shrink");
     }
+  });
 
-    function deleteEffect() {
-        if (currentCharIndex > 0) {
-            element.text(element.text().slice(0, --currentCharIndex));
-            setTimeout(deleteEffect, 50); // Deleting speed in milliseconds
-        } else {
-            currentIndex = (currentIndex + 1) % professions.length;
-            setTimeout(typeEffect, 200); // Wait for a little before starting next title
-        }
+  var professions = [
+    "Full-Stack Developer",
+    "Web Developer",
+    "Software Engineer",
+    "Front-End Developer",
+  ];
+  var currentIndex = 0;
+  var currentCharIndex = 0;
+  var element = $("#profession");
+
+  function typeEffect() {
+    if (currentCharIndex < professions[currentIndex].length) {
+      element.text(
+        element.text() + professions[currentIndex][currentCharIndex++]
+      );
+      setTimeout(typeEffect, 100);
+    } else {
+      setTimeout(deleteEffect, 2000);
     }
+  }
 
-    typeEffect();
+  function deleteEffect() {
+    if (currentCharIndex > 0) {
+      element.text(element.text().slice(0, --currentCharIndex));
+      setTimeout(deleteEffect, 50);
+    } else {
+      currentIndex = (currentIndex + 1) % professions.length;
+      setTimeout(typeEffect, 200);
+    }
+  }
+
+  typeEffect();
 });
